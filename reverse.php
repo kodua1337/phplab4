271c87328729504ce88684bf381ea7a27b2f3956
kodua1337 <kn1b22.kodua@kpnu.edu.ua>
2025-06-22 15:37:20 +0300

    Add files via upload

---
+++ reverse.php
@@ -0,0 +1,13 @@
+﻿<?php
+// Створюємо масив чисел від 1 до 10
+$numbers = range(1, 10);
+
+// Виводимо масив у зворотному порядку
+$reversed = array_reverse($numbers);
+
+echo "<h2>Завдання 1: Реверс масиву</h2>";
+echo "Масив у зворотному порядку: <br>";
+foreach ($reversed as $num) {
+    echo $num . " ";
+}
+?>
